d5b9887829f731d2c66326d4185fe0d6081922c0
xalien073 <95920108+xalien073@users.noreply.github.com>
2024-09-25 17:32:42 +0530

    Added new workflow & updated axios.js!

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
 
+//const baseURL = 'http://localhost:8000';
+// const baseURL = process.env.NEXT_PUBLIC_TMR_API_URL //|| 'https://tmr-api.azurewebsites.net';// Default value
+// const apiURL = 'fastapi-service';
+const apiURL = 'http://fastapi-app-service.tmr.svc.cluster.local:80'
+
+// const baseURL = 'http://10.97.221.13';
 const instance = axios.create({
-  baseURL: 'https://tmr-api.azurewebsites.net',
-  // baseURL: 'http://localhost:7071',
-  // baseURL: 'http://localhost:7072'
+  baseURL: apiURL
 });
 
 export default instance;
+
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: 'https://tmr-api.azurewebsites.net',
+//   // baseURL: 'http://localhost:7071',
+//   // baseURL: 'http://localhost:7072'
+// });
+
+// export default instance;
