0c277a36f7b4dba617c942429757458427e5b151
xalien073 <95920108+xalien073@users.noreply.github.com>
2025-03-15 17:15:55 +0530

    Modified azure-pipeline.yaml & src/axios.js!

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 //const baseURL = 'http://localhost:8000';
 // const baseURL = process.env.NEXT_PUBLIC_TMR_API_URL //|| 'https://tmr-api.azurewebsites.net';// Default value
 // const apiURL = 'fastapi-service';
-const apiURL = 'http://fastapi-app-service.tmr.svc.cluster.local:80'
+// const apiURL = 'http://fastapi-app-service.tmr.svc.cluster.local:80'
+const apiURL = 'http://tmr-api-service.default.svc.cluster.local:80'
 
 // const baseURL = 'http://10.97.221.13';
 const instance = axios.create({
